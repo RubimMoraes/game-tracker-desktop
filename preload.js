2ce620fcdd80b56d9d6bc791093a3485482fa6fa
Rafael Rubim <rrubimmoraes@gmail.com>
2026-04-23 15:08:52 -0300

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("gameLibrary", {
-  loadGames: () => ipcRenderer.invoke("games:load"),
-  saveGames: (games) => ipcRenderer.invoke("games:save", games)
-});
